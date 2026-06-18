500f7ab478fe99368c22bec801abe5253988914b
Ayush24108 <ayushjindal371@gmail.com>
2026-06-18 12:32:05 +0530

    Delete Dbms project queries(1).sql

--- Dbms project queries(1).sql
+++
@@ -1,8 +0,0 @@
-SELECT COUNT(*) INTO v_count
-FROM allocation
-WHERE student_id = p_student_id
-AND exit_time IS NULL;
-
-IF v_count > 0 THEN
-   RAISE_APPLICATION_ERROR(-20003, 'Student already inside');
-END IF;
